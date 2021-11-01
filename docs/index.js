4f6ec00c1d2fb322749e2bd164bd9de017ba3129
Dele Olajide <dele@4ng.net>
2021-11-01 18:24:55 +0000

    wip

--- docs/index.js
+++ docs/index.js
@@ -2,11 +2,11 @@ import JabraSpeak410 from "./jabra-speak-410.js";
 import JabraSpeak510 from "./jabra-speak-510.js";
 import YealinkMP50 from "./yealink-mp50.js";
 
-let jabra = null;
+window.jabra = null;
 
 window.addEventListener("unload", function()
 {
-	if (jabra) jabra.detach();
+	if (window.jabra) window.jabra.detach();
 });
 
 window.addEventListener("load", function()
@@ -20,18 +20,18 @@ window.addEventListener("load", function()
     const events = document.getElementById("events");	
     const device = document.getElementById("device");
 
-    clear.addEventListener('click', 	event => { if (jabra) jabra.clear() });	
-    connect.addEventListener('click', 	event => { if (jabra) jabra.connect() });
-	ring.addEventListener('click', 		event => { if (jabra) jabra.ring() });
-    mute.addEventListener('click', 		event => { if (jabra) jabra.mute() });
-    hold.addEventListener('click', 		event => { if (jabra) jabra.hold() });
+    clear.addEventListener('click', 	event => { if (jabra) window.jabra.clear() });	
+    connect.addEventListener('click', 	event => { if (jabra) window.jabra.connect() });
+	ring.addEventListener('click', 		event => { if (jabra) window.jabra.ring() });
+    mute.addEventListener('click', 		event => { if (jabra) window.jabra.mute() });
+    hold.addEventListener('click', 		event => { if (jabra) window.jabra.hold() });
 	
 	
     attach.addEventListener('click', event => 
 	{
-		jabra = (device.value == "jabra-speak-410") ?  new JabraSpeak410() : ((device.value == "jabra-speak-510") ?  new JabraSpeak510() : new YealinkMP50());			
+		window.jabra = (device.value == "jabra-speak-410") ?  new JabraSpeak410() : ((device.value == "jabra-speak-510") ?  new JabraSpeak510() : new YealinkMP50());			
 		
-		jabra.attach(event => 
+		window.jabra.attach(event => 
 		{
 			console.debug("jabra event", event);
 			events.innerHTML += event + "<br/>"
